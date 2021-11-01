69f2110b0d438e21898c2a83e1f3a64bad0c584c
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2021-11-01 14:11:34 +0300

    location change

--- medicine/src/Components/About/Works.js
+++ medicine/src/Components/About/Works.js
@@ -14,10 +14,10 @@ function Works(){
 
         
             <ul className='works'>
-                <li className='lists'>Mon - Sat- Sun
+                <li className='lists'>Mon - Sat
                 <br />(0800 - 1700hrs)  </li>
                 <li className='lists' ><a  className='a'href='mailto:glowwke@gmail.com'> glowwke@gmail.com </a></li>                
-                <li className='lists' >KTDA PLAZA 5th Flr<br /> Ronald Ngala<br /> Nairobi, Kenya </li>
+                <li className='lists' >KTDA PLAZA 5th Flr<br /> Ronald Ngala</li>
             
             </ul>
         </div>
